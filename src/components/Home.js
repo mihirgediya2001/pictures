d2703c78391abdba63518c3732b6e8750ebd2441
mihirgediya2001 <mihirgediya2001@gmail.com>
2021-07-01 17:34:12 +0530

    add and delete done

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Select from './Select';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Select count={10} />
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ src/components/Select.js
@@ -0,0 +1,128 @@
+import React from 'react';
+
+import './Select.css';
+
+class Select extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.filePickerRef = React.createRef();
+    }
+    state = {
+        files: [null, null, null],
+        position: 0,
+        selectedFile: [],
+        previreUrl: null,
+        isValid: false,
+    };
+
+    pickedHandler = (event) => {
+        if (
+            this.state.position + event.target.files.length <=
+            this.props.count
+        ) {
+            let pickedFile = [];
+            if (event.target.files) {
+                pickedFile = event.target.files;
+                this.setState({
+                    selectedFile: event.target.files,
+                    isValid: true,
+                });
+            }
+
+            console.log(event.target.files, pickedFile, this.state.isValid);
+            console.log(this.state.selectedFile, pickedFile.length);
+            for (var i = 0; i < pickedFile.length; i = i + 1) {
+                console.log(i);
+                const fileReader = new FileReader();
+                fileReader.onload = () => {
+                    let url = fileReader.result;
+                    this.setState({ previewUrl: fileReader.result });
+                    let urls = this.state.files;
+                    if (this.state.position < this.state.files.length) {
+                        urls[this.state.position] = url;
+                    } else {
+                        urls.push(url);
+                    }
+                    this.setState({
+                        files: urls,
+                        position: this.state.position + 1,
+                    });
+                };
+                fileReader.readAsDataURL(pickedFile[i]);
+            }
+        } else {
+            window.alert('Maximum 10 files.');
+        }
+    };
+
+    pickImageHandler = () => {
+        this.filePickerRef.current.click();
+    };
+
+    deletePhotoHandler = (f, i) => {
+        if (this.state.files) {
+            let abc = this.state.files.filter(
+                (f1, index) => f1 !== f || index !== i
+            );
+            this.setState({ files: abc, position: this.state.position - 1 });
+        }
+        this.filePickerRef = React.createRef();
+    };
+
+    render() {
+        return (
+            <div>
+                <div className="images_square">
+                    {this.state.files.map((f, index) => (
+                        <div key={index}>
+                            <div className="square">
+                                <input
+                                    multiple
+                                    type="file"
+                                    style={{ display: 'none' }}
+                                    ref={this.filePickerRef}
+                                    accept=".jpg,.png,.jpeg"
+                                    onChange={this.pickedHandler}
+                                />
+                                <div>
+                                    <div className="image-upload__preview">
+                                        {f && <img src={f} alt="Preview" />}
+                                        {!f && <p>Please pick an image.</p>}
+                                    </div>
+                                    {!f && (
+                                        <button onClick={this.pickImageHandler}>
+                                            Pick photu
+                                        </button>
+                                    )}
+                                    {f && (
+                                        <button
+                                            onClick={() =>
+                                                this.deletePhotoHandler(
+                                                    f,
+                                                    index
+                                                )
+                                            }>
+                                            Delete photu
+                                        </button>
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                    {this.state.position != this.props.count && (
+                        <div>
+                            <button
+                                className="add_more_button"
+                                onClick={this.pickImageHandler}>
+                                Add More
+                            </button>
+                        </div>
+                    )}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Select;
